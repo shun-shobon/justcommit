262de5e4d5d4454dfcc3ac752abe3d851b325327
Shuntaro Nishizawa <me@shun.technology>
2023-03-19 16:59:52 +0900

    feat: Add a sample of making requests to OpenAI

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const token = Deno.env.get("OPENAI_TOKEN");
+const url = new URL("https://api.openai.com/v1/chat/completions");
+const res = await fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+  body: JSON.stringify({
+    "model": "gpt-3.5-turbo",
+    "messages": [{ "role": "user", "content": "Say this is a test!" }],
+    "temperature": 0.7,
+  }),
+});
+const data = await res.json();
+console.log(data);
