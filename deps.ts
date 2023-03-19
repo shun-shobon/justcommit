634f4ea3861fe7cfa61cd59f65154541a7bce493
Shuntaro Nishizawa <me@shun.technology>
2023-03-20 02:33:03 +0900

    feat: Add command parser

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
 export * as z from "https://deno.land/x/zod@v3.21.4/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
+import { Command } from "./deps.ts";
 import { readDiffStagedFiles } from "./git.ts";
 import { generateCommitMessage } from "./generator.ts";
 
-const token = Deno.env.get("OPENAI_TOKEN");
-if (!token) {
-  throw new Error("OPENAI_TOKEN is not set");
-}
+await new Command()
+  .name("justcommit")
+  .description("AI powered commit message generator")
+  .action(async () => {
+    const token = Deno.env.get("OPENAI_TOKEN");
+    if (!token) {
+      throw new Error("OPENAI_TOKEN is not set");
+    }
 
-const diff = await readDiffStagedFiles();
-const commitMessage = await generateCommitMessage(token, diff);
+    const diff = await readDiffStagedFiles();
+    const commitMessage = await generateCommitMessage(token, diff);
 
-console.log(commitMessage);
+    console.log(commitMessage);
+  })
+  .parse(Deno.args);
