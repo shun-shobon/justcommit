c1c23b193e15bede67852440f0544a98b95acfa6
Shuntaro Nishizawa <me@s2n.tech>
2023-08-13 17:18:51 +0900

    feat(main.rs): Implement chat-based commit message generation using ChatGPT

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod args;
 
-use anyhow::{ensure, Result};
+use anyhow::{ensure, Context as _, Result};
 use args::Args;
-// use chatgpt::prelude::*;
+use chatgpt::{prelude::*, types::Role};
 use clap::Parser;
 use git2::{DiffFormat, DiffLineType, DiffOptions, Repository, Tree};
 use glob::Pattern;
+use indoc::indoc;
 use once_cell::sync::Lazy;
 use std::{env, path::PathBuf};
 
@@ -20,28 +21,77 @@ async fn main() -> Result<()> {
     let head_tree = repo.head()?.peel_to_tree()?;
 
     let changed_files = get_changed_files(&repo, &head_tree)?;
-
     ensure!(!changed_files.is_empty(), "No files changed in this commit");
 
     let diffs = get_file_diffs(&repo, &changed_files, &head_tree)?;
 
-    println!("{}", diffs);
+    let openai_token = env::var("OPENAI_TOKEN").context("OPENAI_TOKEN is not set")?;
+
+    let openai_config = ModelConfigurationBuilder::default()
+        .engine(ChatGPTEngine::Gpt35Turbo)
+        .build()
+        .unwrap();
+    let chatgpt_client = ChatGPT::new_with_config(openai_token, openai_config).unwrap();
+
+    // Based on https://github.com/di-sukharev/opencommit/blob/916ddf02d1fcbf6082362e153262e177e39b743b/src/generateCommitMessageFromGitDiff.ts
+    let history = vec![
+        ChatMessage {
+            role: Role::System,
+            content: concat!(
+                "You are to act as the author of a commit message in git. ",
+                "Your mission is to create clean and comprehensive commit messages in the conventional commit convention and explain WHAT were the changes and WHY the changes were done. ",
+                "I'll send you an output of 'git diff --staged' command, and you convert it into a commit message. Do not preface the commit with anything. ",
+                "Don't add any descriptions to the commit, only commit message. ",
+                "Use the present tense. Lines must not be longer than 74 characters. Use english to answer.",
+            ).to_string(),
+        },
+        ChatMessage {
+            role: Role::User,
+            content: indoc! {"
+                diff --git a/src/server.ts b/src/server.ts
+                index ad4db42..f3b18a9 100644
+                --- a/src/server.ts
+                +++ b/src/server.ts
+                @@ -10,7 +10,7 @@
+                import {
+                initWinstonLogger();
+
+                const app = express();
+                -const port = 7799;
+                +const PORT = 7799;
+
+                app.use(express.json());
+
+                @@ -34,6 +34,6 @@
+                app.use((_, res, next) => {
+                // ROUTES
+                app.use(PROTECTED_ROUTER_URL, protectedRouter);
+
+                -app.listen(port, () => {
+                -  console.log(`Server listening on port ${port}`);
+                +app.listen(process.env.PORT || PORT, () => {
+                +  console.log(`Server listening on port ${PORT}`);
+                });
+            "}.to_string(),
+        },
+        ChatMessage {
+            role: Role::Assistant,
+            content: indoc! {"
+                fix(server.ts): Change port variable case from lowercase port to uppercase PORT to improve semantics
+                feat(server.ts): Add support for process.env.PORT environment variable to be able to run app on a configurable port
+            "}.to_string(),
+        },
+        ChatMessage {
+            role: Role::User,
+            content: diffs,
+        }
+    ];
+
+    let resp = chatgpt_client.send_history(&history).await?;
+
+    println!("{}", resp.message().content);
 
     Ok(())
-
-    // let openai_token = env::var("OPENAI_TOKEN").context("OPENAI_TOKEN is not set")?;
-
-    // let openai_config = ModelConfigurationBuilder::default()
-    //     .engine(ChatGPTEngine::Gpt35Turbo)
-    //     .build()
-    //     .unwrap();
-    // let chatgpt_client = ChatGPT::new_with_config(openai_token, openai_config).unwrap();
-
-    // let resp = chatgpt_client.send_message("こんにちは").await?;
-
-    // println!("{}", resp.message().content);
-
-    // Ok(())
 }
 
 fn get_changed_files(repo: &Repository, head_tree: &Tree) -> Result<Vec<PathBuf>> {
