81c51eda3e02645fec2a362ba38e0e1b6f69bcc5
Shuntaro Nishizawa <me@s2n.tech>
2023-08-13 11:44:28 +0900

    chore: Reset all

--- deps.ts
+++
@@ -1,4 +0,0 @@
-export * as path from "https://deno.land/std@0.198.0/path/mod.ts";
-export * as TOML from "https://deno.land/std@0.198.0/encoding/toml.ts";
-export * as z from "https://deno.land/x/zod@v3.21.4/mod.ts";
-export { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
