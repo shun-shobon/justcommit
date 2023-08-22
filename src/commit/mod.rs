94bb3c8a313e7a01b061700b405cc05287390867
Shuntaro Nishizawa <me@s2n.tech>
2023-08-23 03:35:46 +0900

    feat: Add commit module for generating commit messages from git diffs

---
+++ src/commit/mod.rs
@@ -0,0 +1,124 @@
+mod openai;
+
+use std::fmt::Display;
+
+use self::openai::{Function, Message, Role};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+pub async fn generate_message(
+    openai_token: &str,
+    diffs: impl Into<String> + Send,
+) -> anyhow::Result<String> {
+    let mut commit_func_params = schemars::schema_for!(CommitMessage);
+    commit_func_params.meta_schema = None;
+
+    let commit_func = Function {
+        name: "set_commit_message",
+        description: Some("Sets the commit message"),
+        parameters: serde_json::to_value(commit_func_params)?,
+    };
+
+    let messages = create_history(diffs);
+    let message = openai::send_request(openai_token, Some(&[commit_func]), &messages).await?;
+    let Some(function_call) = message.function_call else {
+        anyhow::bail!("No function call returned from OpenAI API")
+    };
+
+    let commit_args = serde_json::from_str::<CommitMessage>(&function_call.arguments)?;
+    Ok(commit_args.to_string())
+}
+
+fn create_history(diffs: impl Into<String>) -> Vec<Message> {
+    // Based on https://github.com/di-sukharev/opencommit/blob/916ddf02d1fcbf6082362e153262e177e39b743b/src/generateCommitMessageFromGitDiff.ts
+    vec![
+        Message {
+            role: Role::System,
+            content: Some(concat!(
+                "You are to act as the author of a commit message in git. ",
+                "Your mission is to create clean and comprehensive commit messages in the conventional commit convention and explain WHAT were the changes and WHY the changes were done. ",
+                "I'll send you an output of 'git diff --staged' command, and you convert it into a commit message. ",
+                "Do not preface the commit with anything. ",
+                "Don't add any descriptions to the commit, only commit message. ",
+                "Use the present tense. ",
+                "Lines must not be longer than 74 characters. ",
+                "Use english to answer.",
+            ).to_string()),
+            function_call: None,
+        },
+        Message {
+            role: Role::User,
+            content: Some(diffs.into()),
+            function_call: None,
+        }
+    ]
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
+struct CommitMessage {
+    /// The type of the commit
+    #[serde(rename = "type")]
+    commit_type: CommitType,
+
+    /// The subject of the commit
+    message: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "lowercase")]
+enum CommitType {
+    /// A new feature
+    Feat,
+
+    /// A bug fix
+    Fix,
+
+    /// Changes that affect the build system or external dependencies
+    Build,
+
+    /// Changes to our CI configuration files and scripts
+    Ci,
+
+    /// Documentation only changes
+    Docs,
+
+    /// A code change that improves performance
+    Perf,
+
+    /// A code change that neither fixes a bug nor adds a feature
+    Refactor,
+
+    /// Changes that do not affect the meaning of the code
+    Style,
+
+    /// Adding missing tests or correcting existing tests
+    Test,
+
+    /// Other changes that don't modify src or test files
+    Chore,
+}
+
+impl Display for CommitMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.commit_type, self.message)
+    }
+}
+
+impl Display for CommitType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Self::Feat => "feat",
+            Self::Fix => "fix",
+            Self::Build => "build",
+            Self::Ci => "ci",
+            Self::Docs => "docs",
+            Self::Perf => "perf",
+            Self::Refactor => "refactor",
+            Self::Style => "style",
+            Self::Test => "test",
+            Self::Chore => "chore",
+        };
+
+        write!(f, "{s}")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 mod args;
+mod commit;
 mod config;
 mod env;
 mod git;
-mod openai;
 
 use anyhow::Result;
 use args::Args;
@@ -13,7 +13,6 @@ use clap::Parser;
 use crate::config::Config;
 use crate::env::Env;
 use crate::git::get_diffs;
-use crate::openai::generate_commit_message;
 
 static APP_NAME: &str = env!("CARGO_PKG_NAME");
 
@@ -24,7 +23,7 @@ async fn main() -> Result<()> {
     let env = Env::create(&args, config)?;
 
     let diffs = get_diffs()?;
-    let message = generate_commit_message(&env.openai_token, diffs).await?;
+    let message = commit::generate_message(&env.openai_token, diffs).await?;
 
     println!("{message}");
 
